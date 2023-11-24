7656a90beeb3cd6e729a3a6ffe8811cd521eb4dc
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-11-24 19:39:55 +0530

    Love babbar sir

---
+++ class and object .cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class hero{
+    
+   //  properties
+   int health;
+    
+};
+
+int main()
+{
+    
+    // create a object of hero type
+    hero h1;            // h1 take the size of int health int value of 4
+    cout<<"size : "<< sizeof(h1)<<endl;
+    
+}
+
+
+// in empty  class  we will get 1 bit memory 
+
+// class hero{
+    // properties empty here 
+ //  }
